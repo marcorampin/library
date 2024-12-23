4963febd902b75e0f079012723f66645996c183f
Marco Rampin <rampin.marco.17@gmail.com>
2024-12-23 16:15:16 +0100

    Prints a card for each book with its info by using several JS functions

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let div = document.querySelector('.prova');
+let shelves = document.querySelector('.shelves');
 //div.innerText = 'Ciao0';
 
 const myLibrary = [];
 
-function Book (title, author, pages, read) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -15,14 +15,39 @@ function addBookToLibrary(title, author) {
     myLibrary.push(book);
 }
 
-function printLibrary (){
+function printLibrary(){
     for (const book of myLibrary) {
-	console.log(book);
+	createCard(book);
     }
+}
+
+function createCard(book) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.setAttribute('id', myLibrary.indexOf(book));
+    shelves.appendChild(card);
+    for (const key of Object.keys(book)) {
+	if (key === 'title') {
+	    addBookInfo(book, card, 'title', 'h2', key, book[key]);
+	}
+	else {
+	    addBookInfo(book, card, 'p', 'key', key, key);
+	    addBookInfo(book, card, 'p', 'value', key, book[key]);
+	}
+    }
+}
+
+function addBookInfo(book, card, el, elClass, key, value) {
+    const element = document.createElement(el);
+    element.classList.add(elClass);
+    element.textContent = value;
+    card.appendChild(element);
 }
 
 addBookToLibrary('Hobbit', 'JFK');
 addBookToLibrary('Potter', 'Rowlings');
 addBookToLibrary('Foundation', 'Asimov');
 addBookToLibrary('Poirot', 'Christie');
+addBookToLibrary('Cicciomerda', 'Cicciogamer89');
+addBookToLibrary('Cicciomerda2', 'Cicciogamer89');
 printLibrary();
